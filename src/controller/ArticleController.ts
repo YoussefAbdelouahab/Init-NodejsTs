d12ee245d5934c8a9c9c6e27b5471f943acef231
YoussefAbdelouahab <abdelo_y@etna-alternance.net>
2023-02-27 17:44:41 +0100

    add storing array file

--- src/controller/ArticleController.ts
+++ src/controller/ArticleController.ts
@@ -1,4 +1,4 @@
-import { JsonController, Body, Post, UploadedFile, Get, Param, Req, UseBefore } from 'routing-controllers';
+import { JsonController, Body, Post, UploadedFile, Get, Param, Req, UseBefore, Put, UploadedFiles } from 'routing-controllers';
 import { AppDataSource } from '../db/data-source';
 import { Article } from '../entity/Article';
 import { File } from '../entity/File';
@@ -15,7 +15,7 @@ export class ArticleController {
 
     @Post('/article')
     @UseBefore(UserAuthMiddelware)
-    async CreateArticle(@Body() data: Article, @Body() fileData: File, @UploadedFile("url", { options: multerConfig }) storedFile: any, @Req() req: any) {
+    async CreateArticle(@Body() data: Article, @Body() fileData: File, @UploadedFiles("url", { options: multerConfig }) storedFile: Array<any>, @Req() req: any) {
         try {
             /*Initialising objects and variables*/
             const article: Article = data;
@@ -29,7 +29,11 @@ export class ArticleController {
 
             /*Saving datas*/
             await this.articleRepository.save(article);
-            await this.fileRepository.save({...file, article: article, url: storedFile.filename});
+            if(storedFile.length > 1){
+                storedFile.forEach(element => {
+                    this.fileRepository.save({...file, article: article, url: element.filename});
+                });
+            }
 
             /*Return response*/
             return { success: "Article created" };
@@ -43,7 +47,30 @@ export class ArticleController {
     async getArticle(@Param('id') id: string) {
         try{
             const article: Article = await this.articleRepository.findOne({ where: {id: id} })
-            return { article : article }
+            if (!article) throw new Error('Article not found');
+            const file: File = await this.fileRepository.find({
+                relations: ["article"],
+                where: { article: { id: article.getId()} }
+            })
+            if (!file) throw new Error('File not found');
+            return { article : article, file }
+        } catch(err){
+            return { error: err.message }
+        }
+    }
+
+    @Put('/article/:id')
+    async updateArticle(@Body() data: Article, @Param('id') id: string) {
+        try{
+            const article: Article = await this.articleRepository.findOne({ where: {id: id} })
+            if (!article) throw new Error('Article not found');
+            const file: File = await this.fileRepository.find({
+                relations: ["article"],
+                where: { article: { id: article.getId()} }
+            })
+            if (!file) throw new Error('File not found');
+            console.log(article, file);
+            return { success : article, file }
         } catch(err){
             return { error: err.message }
         }
